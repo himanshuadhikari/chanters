5ae469a2a498e5ece8514cdba0c93ad454fc41e7
himanshu-bluepi <himanshu@bluepi.in>
2015-11-02 01:18:11 +0530

    chanters is on for people

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ app.use(express.static(__dirname + '/public'));
 app.engine('html', require('ejs').renderFile);
 
 
+// app.get('/', function(req, res) {
+//     res.render('index.html');
+// });
 app.get('/', function(req, res) {
-    res.render('index.html');
+    res.render(res.render(__dirname + '/public/view/player.html'););
 });
 app.get('/development', function(req, res) {
     res.render(__dirname + '/public/view/development.html');
